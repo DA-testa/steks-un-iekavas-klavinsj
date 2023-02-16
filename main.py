4a470892f5cdb299498e7e151775cddeb1f2808c
Jānis Kļaviņš <124731554+klavinsj@users.noreply.github.com>
2023-02-16 18:41:25 +0000

    tezsc

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
-            # Process opening bracket, write your code here
+            # Process opening bracket, write your code here  
             opening_brackets_stack.append(Bracket(next,i+1))
 
             
@@ -22,9 +22,9 @@ def find_mismatch(text):
         if next in ")]}":
             if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
                 return i+1
-            opening_brackets_stack.pop()
-    if opening_brackets_stack:
-        return opening_brackets_stack[-1].position
+            opening_brackets_stack.pop() 
+    if  opening_brackets_stack:
+        return opening_brackets_stack[-1].position 
     return "Success"
             # Process closing bracket, write your code here
             
@@ -32,9 +32,14 @@ def find_mismatch(text):
 
 def main():
     text = input()
-    mismatch = find_mismatch(text)
-    # Printing answer, write your code here
-    print(mismatch)
+
+            
+
+    if "I" or "i" in text:
+        text = input()
+        mismatch = find_mismatch(text)
+        # Printing answer, write your code here
+        print(mismatch)
 
 if __name__ == "__main__":
     main()
